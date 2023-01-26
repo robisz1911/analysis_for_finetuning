402447c4112e56bc98d713dec4eddeeaf45d2fd8
robisz1911 <robisz1911@gmail.com>
2023-01-26 19:49:24 +0100

    Add plot method - plot avg RBG values for each neuron per layer

--- image_collection.py
+++ image_collection.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 import urllib.request
 from skimage import io
 import numpy as np
+import matplotlib.pyplot as plt
 
 def sub_folder_round(number):
     if int(number + 1) < 20:
@@ -77,7 +78,8 @@ def avg_colour_for_each_neuron(finetuned):
     layers = get_layers()
     avgs = []
     for idx, [layer, size] in tqdm(enumerate(layers)):
-        avg_colour_by_neuron_in_layer = []
+        avg_rgb_colour_for_layer_per_neuron = np.zeros([int(size),3])
+
         for neuron in range(int(size)):
             if finetuned:
                 postfix = "_finetuned"
@@ -85,22 +87,60 @@ def avg_colour_for_each_neuron(finetuned):
             else:
                 postfix = "_default"
                 name = layer + "_default.npy"
+
             layer_obj = np.load("layerObjs/" + name)
+
             red  = layer_obj[neuron,:,:,0].mean()
             green = layer_obj[neuron,:,:,1].mean()
             blue = layer_obj[neuron,:,:,2].mean()
-            avg_colour_by_neuron_in_layer.append((red, green, blue))
-        avgs.append(avg_colour_by_neuron_in_layer)
+            
+            avg_rgb_colour_for_layer_per_neuron[idx, :] =  np.array([red,green,blue])
+
+        avgs.append(avg_rgb_colour_for_layer_per_neuron)
     np.save("avg_col_by_neuron_for_ea_layer" + postfix, avgs)
 
-def check_avgs():
-    #x = np.load("avg_col_by_neuron_for_ea_layer_default.npy", allow_pickle=True)
     x = np.load("layerObjs/Mixed_5b_Branch_3_b_1x1_act_finetuned.npy")
     print(x.shape)
     print(x[1,1,1,:])
     x = np.load("avg_col_by_neuron_for_ea_layer_default.npy", allow_pickle=True)
     print(x.shape)
     print(x)
+
+def plot_avg_col():
+    layers = get_layers()
+    avgs = []
+
+    default = np.load("avg_col_by_neuron_for_ea_layer_default.npy", allow_pickle=True)
+    finetuned = np.load("avg_col_by_neuron_for_ea_layer_finetuned.npy", allow_pickle=True)
+
+    for idx, [layer, size] in tqdm(enumerate(layers)):
+        default_red  = default[idx][:,0]
+        default_green = default[idx][:,1]
+        default_blue = default[idx][:,2]
+
+        finetuned_red = finetuned[idx][:,0]
+        finetuned_green = finetuned[idx][:,1]
+        finetuned_blue = finetuned[idx][:,2]
+
+
+        plt.plot(range(size), red, label='red_d', color='r', linewidth=3)
+        plt.plot(range(size), green, label='green_d', color='g', linewidth=3)
+        plt.plot(range(size), blue, label='blue_d', color='b', linewidth=3)
+
+        plt.plot(range(size), red_f, '-.', label='red_f', color='r', linewidth=3)
+        plt.plot(range(size), green_f, '-.', label='green_f', color='g', linewidth=3)
+        plt.plot(range(size), blue_f, '-.', label='blue_f', color='b', linewidth=3)
+
+        plt.title(layer)
+        plt.ylabel('colour_value')
+        plt.xlabel('neuron')
+        plt.legend()
+        for i in range(10):
+            plt.axvline(x=i)
+
+        plt.xticks(range(10))
+        plt.rcParams['figure.figsize'] = (4, 15)
+        plt.savefig("plots/" + layer + "avgs" + '.png')
 
 
 
@@ -117,5 +157,5 @@ def check_avgs():
 # default imgs to numpy obj
 #save_imgs_to_numpy(False)
 
-#avg_colour_for_each_neuron(True)
-#avg_colour_for_each_neuron(False)
+avg_colour_for_each_neuron(True)
+avg_colour_for_each_neuron(False)
