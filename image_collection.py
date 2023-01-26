daeb23d14d70926ac3ee43f9366b5a5231f4fa77
robisz1911 <robisz1911@gmail.com>
2023-01-27 00:28:47 +0100

    Avg color change by layers

--- image_collection.py
+++ image_collection.py
@@ -135,24 +135,76 @@ def plot_avg_col():
         plt.savefig("plots/" + layer + "avgs" + '.png', dpi = 100)
         plt.close()
 
+def square_diff_of_avgs():
+    layers = get_layers()
+    
+    square_diff_by_color = {"red" : [], "green" : [], "blue" : []}
+
+    default = np.load("avg_col_by_neuron_for_ea_layer_default.npy", allow_pickle=True)
+    finetuned = np.load("avg_col_by_neuron_for_ea_layer_finetuned.npy", allow_pickle=True)
+    for idx, [layer, size] in tqdm(enumerate(layers)):
+        default_red  = default[idx][:,0]
+        default_green = default[idx][:,1]
+        default_blue = default[idx][:,2]
+
+        finetuned_red = finetuned[idx][:,0]
+        finetuned_green = finetuned[idx][:,1]
+        finetuned_blue = finetuned[idx][:,2]
+        
+        # calc square diff of avgs, and mean it over layer
+#        sq_diff_red = np.absolute(default_red - finetuned_red).mean()
+#        sq_diff_green = np.absolute(default_green - finetuned_green).mean()
+#        sq_diff_blue = np.absolute(default_blue - finetuned_blue).mean()
+
+        sq_diff_red = np.square(default_red - finetuned_red).mean()
+        sq_diff_green = np.square(default_green - finetuned_green).mean()
+        sq_diff_blue = np.square(default_blue - finetuned_blue).mean()
+
+
+        square_diff_by_color["red"].append(sq_diff_red)
+        square_diff_by_color["green"].append(sq_diff_green)
+        square_diff_by_color["blue"].append(sq_diff_blue)
+
+    # create plots
+    plt.figure(figsize=(100, 5), dpi=1)
+
+    plt.plot(range(len(square_diff_by_color["red"])), square_diff_by_color["red"], label='red', color='r', linewidth=1)
+    plt.plot(range(len(square_diff_by_color["green"])), square_diff_by_color["green"], label='green', color='g', linewidth=1)
+    plt.plot(range(len(square_diff_by_color["blue"])), square_diff_by_color["blue"], label='blue', color='b', linewidth=1)
+
+    plt.title("Average color change by layer")
+    plt.ylabel('mean of square diffs over all neurons')
+    plt.xlabel('layer')
+    plt.legend()
+    for i in range(len(square_diff_by_color["blue"])):
+        plt.axvline(x=i)
+    plt.xticks(range(len(square_diff_by_color["blue"])))
+    plt.savefig("stats/" + "Average_color_change_by_layer_abs_diff" + '.png', dpi = 100)
+    plt.close()
+
+
+
 
 
 ##################################################################################################
 
 # download imgs from ai.reny.hu to local
-download_imgs()
+#download_imgs()
 
 # finetuned imgs to numpy obj (layer by layer)
-save_imgs_to_numpy(True)
+#save_imgs_to_numpy(True)
 
 # default imgs to numpy obj
-save_imgs_to_numpy(False)
+#save_imgs_to_numpy(False)
 
 # finetuned
-avg_colour_for_each_neuron(True)
+#avg_colour_for_each_neuron(True)
 
 # default
-avg_colour_for_each_neuron(False)
+#avg_colour_for_each_neuron(False)
 
 # plot
-plot_avg_col()
+#plot_avg_col()
+
+# stat 
+square_diff_of_avgs()
